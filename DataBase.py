61098d30c88cb74e9e275c33375ee3a6bed97b14
Yudai Hayashi <yudai0913haya@gmail.com>
2019-04-30 16:30:15 +0900

    change everything

--- DataBase.py
+++ DataBase.py
@@ -1,280 +1,56 @@
+import psycopg2
 import os
-import sqlite3 as sq
-
-import PDFParse
-import WebSCraping
-
 
 
 class PaperDataBase:
+    def __init__(self, dbname, user):
+        self.connect = psycopg2.connect(f"dbname={dbname} user={user}")
+        self.cursor = self.connect.cursor()
 
-    def __init__(self):
-        self.db, self.c = self.__set_DB()
-        self.__set_Table()
+        self.create_table()
 
+    def create_table(self):
+        self.cursor.execute(
+            "create table if not exists paper_list( \
+                title varchar(100) not null, \
+                authors varchar(200), \
+                journal_name varchar(100), \
+                keywords varchar(200), \
+                primary key (title) \
+            );"
+        )
 
+    def register(self, title, author, journal, keyword):
+        title = title if title != '' else None
+        self.cursor.execute(
+            "insert into paper_list (title, authors, journal_name, keywords) values \
+            (%s, %s, %s, %s);",
+            (title, author, journal, keyword)
+        )
 
-    def __set_DB(self):
-        db = sq.connect('JData.db')
-        c = db.cursor()
+        self.connect.commit()
+        self.connect.close()
 
-        return db, c
-
-
-
-    def __set_Table(self):
-        try:
-            self.c.execute("create table Journal(FileName, URL, Title, Authors, Year, JName, Vol, Abst);")
-        except sq.OperationalError:
-            pass
-
-
-
-    def SearchDB(self, FileName, Author, KeyWords):
-        """
-        Connect to SetResult in main.py and connect to Search button
-        :param FileName: File name of .pdf
-        :param Author: Authors' name or empty
-        :param KeyWords: Key words' list
-        :return: String list shows FileName. We will also use this result to open PDF file
-        """
+    def search(self, title, author, journal, keyword):
+        query = "select * from paper_list \
+                    where title = %s \
+                    and authors = '*'; \
+                "
+        self.cursor.execute(query, (title,))
         result = []
+        for c in self.cursor:
+            result.append(c[0])
 
-        if FileName != '':
-            self.c.execute("select * from Journal where FileName like " + "'%" + FileName + "%'")
-            for row in self.c:
-                result.append(row[0])
-            return result
-
-        if Author != "":
-            condition = "Authors like " +"'%" + Author + "%'"
-        else:
-            condition = ""
-
-        if KeyWords != ['']:
-            for i in range(len(KeyWords)):
-                if i == 0:
-                    condition += "Abst like " + "'%" + KeyWords[i] + "%'"
-                else:
-                    condition += " and Abst like " + "'%" + KeyWords[i] + "%'"
-
-        self.c.execute("select * from Journal where " + condition)
-        for row in self.c:
-            result.append(row[0])
+        self.connect.close()
 
         return result
 
 
-
-    def RegistDB(self, FileName, URL):
-        """
-        Regist (FileName, URL, Title, AuthorsName, Year, JournalName, Vol., Abstract) to DataBase
-        If could not get paper's info, raise AssertionError
-        :param FileName: File name of .pdf
-        :param URL: URL to the info page of the paer
-        :return: None. Registrate the journal info to DataBase
-        """
-
-        StrList = list
-        sq.register_adapter(StrList, lambda l: ';'.join([str(i) for i in l]))
-        sq.register_converter("StrList", lambda s: [str(i) for i in s.split(';')])
-
-
-        if URL == '':
-            PDF_Directory = PDFPath().SearchDB(FileName)
-            URL = PDFParse.URL(PDF_Directory).get_URL()
-        if URL == -1:
-            raise AssertionError
-
-        Scrape = WebSCraping.SortJournal(URL)
-
-        Title = Scrape.get_title()
-        AuthorName = Scrape.get_authors()
-        Year = Scrape.get_year()
-        JournalName = Scrape.get_JName()
-        Vol = Scrape.get_Vol()
-        Abstract = Scrape.get_Abst()
-
-        if Title == -1 or AuthorName == -1 or Year == -1 or JournalName == -1 or Vol == -1 or Abstract == -1:
-            raise AssertionError
-
-        self.c.execute('insert into Journal values(?,?,?,?,?,?,?,?)', (FileName, URL, Title, AuthorName, Year,
-                                                                         JournalName, Vol, Abstract))
-        self.db.commit()
-
-        return None
-
-
-
-    def paper_list(self, path):
-        """
-        :param path: path to paper folder
-        :return: list of papers
-        """
-        result = []
-        for dir in os.listdir(path):
-            if os.path.isdir(path + "/" + dir):
-                result.extend(self.paper_list(path + "/" + dir))
-            elif "pdf" in dir:
-                result.append((path +  '\\' + dir).replace('\\', '/'))
-
-        return result
-
-
-
-    def NotRregistrated(self, path):
-        """
-        Find PDF file not registrated in the PDF_path
-        :return: List
-        """
-        # path = FilePath().get_path()
-        registered = []
-        self.c.execute('select * from Journal')
-        for row in self.c:
-            registered.append(row[0])
-
-        return list(set(self.paper_list(path)) - set(registered))
-
-
-
-    def DeletedAlready(self, path):
-        """
-        Find PDF files deleted already from directory but still registered in the PDF_path.
-        :return: List
-        """
-        registered = []
-        self.c.execute('select * from Journal')
-        for row in self.c:
-            registered.append(row[1])
-
-        return list(set(registered) - set(self.paper_list(path)))
-
-
-
-
-
-class FilePath:
-    """
-    Deta base to store the paths to paper folder.
-    DataBase name : FilePath.db
-    Table name : FilePath
-    """
-
-    def __init__(self):
-        self.db, self.c = self.__set_DB()
-        self.set_Table()
-
-
-
-    def __set_DB(self):
-        db = sq.connect('FilePath.db')
-        c = db.cursor()
-
-        return db, c
-
-
-
-    def set_Table(self):
-        try:
-            self.c.execute("create table FilePath(path);")
-        except sq.OperationalError:
-            pass
-
-
-
-    def is_empty(self):
-        """
-        If the DataBase of file path to papers is empty or there isn't FilePath.db, return True.
-        :return: Boolean
-        """
-        self.c.execute('select * from FilePath')
-        PathList = []
-        for row in self.c:
-            PathList.append(row)
-        if PathList: return False
-
-        return True
-
-
-
-    def add_path(self, path):
-        sql = "insert into FilePath values(?);"
-        self.c.execute(sql, [path])
-        self.db.commit()
-
-        return None
-
-
-
-    def get_path(self):
-        self.c.execute('select * from FilePath')
-        result = []
-        for row in self.c:
-            result.append(row[0])
-
-        return result
-
-
-
-    def clear(self, item):
-        item = "'" + item + "'"
-        self.c.execute("delete from FilePath where path=" + item)
-        self.db.commit()
-
-
-
-    def close(self):
-        self.db.close()
-
-
-
-
-
-
-class PDFPath:
-    """
-    Data base to store paths to all paper in paper folders.
-    DataBase name : PDFPath.db
-    table name : PDF_path
-    """
-    def __init__(self):
-        self.db, self.c = self.__set_DB()
-        self.__set_Table()
-
-
-
-    def __set_DB(self):
-        db = sq.connect('PDFPath.db')
-        c = db.cursor()
-
-        return db, c
-
-
-
-    def __set_Table(self):
-        try:
-            self.c.execute('create table PDF_path(FileName, Path);')
-        except sq.OperationalError:
-            pass
-
-
-
-    def SearchDB(self, FileName):
-        """
-        Return absolute path of a paper. Using this path, open PDF with os.popen(path) connected to QListWidget.
-        :param FileName: PDF File Name
-        :return: Absolute Path to the FileName
-        """
-        FileName += ".pdf"
-        self.c.execute("select * from PDF_path where FileName=" + "'" + FileName +"'")
-        for row in self.c:
-            return row[1]
-
-
-
-    def RegistDB(self, FileName, Path):
-        self.c.execute('insert into PDF_path values(?, ?)', (FileName, Path))
-        self.db.commit()
-
-    def clear(self):
-        self.c.execute('delete from PDF_path')
+if __name__ == '__main__':
+    conn = psycopg2.connect("dbname=test user=yudai")
+    cur = conn.cursor()
+    cur.execute("drop table paper_list;")
+    conn.commit()
+    conn.close()
+    #cur.execute('select * from paper_list;')
+    # print(cur.fetchall())
--- main.py
+++ main.py
@@ -1,28 +1,23 @@
-#***********************************Future Plan*************************************************
+# ***********************************Future Plan*************************************************
 # Compile abstract info with LaTeX and show in a panel
 # Setting some keyword and notificate periodically new papers relate to those words
-#***********************************************************************************************
+# ***********************************************************************************************
 
 from PyQt5.QtWidgets import (QAction, QApplication, QGridLayout, QHBoxLayout, QMainWindow, QMessageBox,
                              QLabel, QLineEdit, QListWidget, QPushButton, QTextEdit, QVBoxLayout, QWidget)
 from PyQt5.QtGui import QIcon
 import os
+import psycopg2
 
 import DataBase
 import LaTeX
 
 
-
 class MainWindow(QMainWindow):
-
     def __init__(self):
         super().__init__()
         self.initUI()
         self.setWindowIcon(QIcon('icon.png'))
-        if DataBase.FilePath().is_empty():
-            self.SetFilePath()
-
-
 
     def initUI(self):
         ############################################
@@ -43,8 +38,6 @@ class MainWindow(QMainWindow):
         centralWidget.setLayout(MainLayout)
         self.setCentralWidget(centralWidget)
 
-
-
         ############################################
         # Menubar Layout
         ############################################
@@ -53,57 +46,43 @@ class MainWindow(QMainWindow):
         exitAction.setStatusTip('Exit application')
         exitAction.triggered.connect(self.close)
 
-        registerAction = QAction('Auto Register', self)
-        registerAction.triggered.connect(self.AutoRegistration)
+        #registerAction = QAction('Auto Register', self)
+        # registerAction.triggered.connect(self.AutoRegistration)
 
         pathAction = QAction('Path', self)
-        pathAction.triggered.connect(self.SetFilePath)
-        SetPDFPath = QAction('Set PDF Path', self)
-        SetPDFPath.triggered.connect(self.RegisterPDFPath)
 
         menubar = self.menuBar()
         fileMenu = menubar.addMenu('&File')
         pathmenu = menubar.addMenu('&Setting')
 
         fileMenu.addAction(exitAction)
-        fileMenu.addAction(registerAction)
-        pathmenu.addAction(pathAction)
-        pathmenu.addAction(SetPDFPath)
+        # fileMenu.addAction(registerAction)
+        # pathmenu.addAction(pathAction)
 
         self.setGeometry(300, 100, 600, 400)
         self.setWindowTitle('PaperResearcher')
         self.show()
 
-
-
-    def SetFilePath(self):
-        self.ex = SetPath()
-        self.ex.show()
-
-
-
     def TextEntrance(self):
-        self.FileName = QLineEdit()
+        self.Title = QLineEdit()
         self.Author = QLineEdit()
         self.Journal = QLineEdit()
         self.KeyWords = QLineEdit()
         self.URL = QLineEdit()
 
         Text_Entrance = QGridLayout()
-        Text_Entrance.addWidget(QLabel('File Name :'), 0, 0)
+        Text_Entrance.addWidget(QLabel('Title :'), 0, 0)
         Text_Entrance.addWidget(QLabel('Author :'), 1, 0)
         Text_Entrance.addWidget(QLabel('Journal :'), 2, 0)
         Text_Entrance.addWidget(QLabel('Key Words :'), 3, 0)
         Text_Entrance.addWidget(QLabel('URL :'), 4, 0)
-        Text_Entrance.addWidget(self.FileName, 0, 1)
+        Text_Entrance.addWidget(self.Title, 0, 1)
         Text_Entrance.addWidget(self.Author, 1, 1)
         Text_Entrance.addWidget(self.Journal, 2, 1)
         Text_Entrance.addWidget(self.KeyWords, 3, 1)
         Text_Entrance.addWidget(self.URL, 4, 1)
 
         return Text_Entrance
-
-
 
     def Buttons(self):
         buttons = QVBoxLayout()
@@ -116,11 +95,9 @@ class MainWindow(QMainWindow):
 
         SearchBottun.clicked.connect(self.Search)
         RegistrationButton.clicked.connect(self.Registration)
-        LaTeXButton.clicked.connect(self.LaTeX)
+        # LaTeXButton.clicked.connect(self.LaTeX)
 
         return buttons
-
-
 
     def TextBox(self):
         self.search_phrase = QLabel('Search Result :')
@@ -141,189 +118,40 @@ class MainWindow(QMainWindow):
         text.addLayout(Search_items)
         text.addLayout(TeXCopy)
 
-        self.Search_result.itemDoubleClicked.connect(lambda item : self.OpenPDF(item))
+        """
+        self.Search_result.itemDoubleClicked.connect(
+            lambda item: self.OpenPDF(item))
         self.CopyButton.clicked.connect(self.copy_tex)
+        """
 
         return text
 
+    def Registration(self):
+        title = self.Title.text()
+        authors = self.Author.text()
+        journal = self.Journal.text()
+        keyword = self.KeyWords.text()
 
-
-    def get_KeyWord(self):
-        """
-        Transform entered KeyWords to a list of string
-        :return: String List of KeyWords
-        """
-        KeyWord = self.KeyWords.text()
-
-        if ', ' in KeyWord:
-            return KeyWord.split(', ')
-        if ',' in KeyWord:
-            return KeyWord.split(',')
-
-        return KeyWord.split(' ')
-
-
+        db = DataBase.PaperDataBase('test', 'yudai')
+        try:
+            db.register(title, authors, journal, keyword)
+        except psycopg2.errors.NotNullViolation:
+            QMessageBox.warning(
+                self, 'Message', 'The title should not be empty'
+            )
 
     def Search(self):
-        FileName = self.FileName.text() #get FileName from FileName in TextEntrance
-        Author = self.Author.text()     #get Authors from Author in TextEntrance
-        KeyWords = self.get_KeyWord()   #String List of KeyWords
+        title = self.Title.text()
+        authors = self.Author.text()
+        journal = self.Journal.text()
+        keyword = self.KeyWords.text()
 
-        try:
-            self.SetResult(FileName, Author, KeyWords)
-        except:
-            return QMessageBox.information(self, 'Message', 'Cannot find such papers')
+        db = DataBase.PaperDataBase('test', 'yudai')
+        result = db.search(title, authors, journal, keyword)
 
-
-
-    def Registration(self):
-        FileName = self.FileName.text()
-        URL = self.URL.text()
-        try:
-            DataBase.PaperDataBase().RegistDB(FileName, URL)
-            return QMessageBox.information(self, 'Message', 'Registered journal correctly')
-        except:
-            return QMessageBox.information(self, 'Message', 'Cannot Register journal info')
-
-
-
-    def AutoRegistration(self):
-        #TODO : Find papers have not been registered yet
-        #TODO : Regisit these papers
-        return None
-
-
-
-    def LaTeX(self):
-        FileName = self.FileName.text()
-        try:
-            self.LateX_result.setText(LaTeX.LaTeX().ToLaTeX(FileName))
-        except:
-            return QMessageBox.information(self, 'Message', 'Cannot create TeX format')
-
-
-
-    def SetResult(self, FileName, Author, KeyWords):
-        """
-        Set Search result to Search_result. If cannnot find anything, raise AssertionError
-        :param FileName:
-        :param Author:
-        :param KeyWords:
-        :return: None
-        """
-        research_result = DataBase.PaperDataBase().SearchDB(FileName, Author, KeyWords)
         self.Search_result.clear()
-        if research_result != []:
-            for item in research_result:
-                self.Search_result.addItem(item)     #show only FileName
-        else:
-            raise AssertionError
-
-
-
-    def OpenPDF(self, item):
-        pdf_path = DataBase.PDFPath().SearchDB(item.text())
-        os.popen(pdf_path)
-
-
-
-    def copy_tex(self):
-        self.LateX_result.selectAll()
-        self.LateX_result.copy()
-
-        return None
-
-
-
-    def RegisterPDFPath(self):
-        paths  = DataBase.FilePath().get_path()
-        pdf  = DataBase.PDFPath()
-        try:
-            pdf.clear()
-        except:
-            pass
-        papers = DataBase.PaperDataBase()
-        for path in paths:
-            paper_list = papers.paper_list(path)
-            for item in paper_list:
-                pdf.RegistDB(item.split('/')[-1], item)
-
-        return QMessageBox.information(self, 'Message', 'Registered All PDF Paths Correctly')
-
-
-
-
-
-class SetPath(QWidget):
-
-    def __init__(self):
-        super().__init__()
-        self.setWindowIcon(QIcon('icon.png'))
-        self.initUI()
-        self.showPath()
-
-    def initUI(self):
-        PathEntrance = QGridLayout()
-        Phrase = QLabel('Setting the paper folder')
-        self.PathSpace = QLineEdit()
-        self.PathPhrase = QLabel('Registerd Path :')
-        self.PathList = QListWidget()
-        PathEntrance.addWidget(Phrase,0,0)
-        PathEntrance.addWidget(self.PathSpace, 1, 0)
-        PathEntrance.addWidget(self.PathPhrase, 2, 0)
-        PathEntrance.addWidget(self.PathList, 3, 0)
-
-        Buttons = QHBoxLayout()
-        self.OKButton = QPushButton('OK')
-        self.CancelButton = QPushButton('Cancel')
-        self.DeletePath = QPushButton('Delete Path')
-        Buttons.addWidget(self.OKButton)
-        Buttons.addWidget(self.CancelButton)
-        Buttons.addWidget(self.DeletePath)
-
-        self.OKButton.clicked.connect(self.RegistPath)
-        self.CancelButton.clicked.connect(self.close)
-        self.DeletePath.clicked.connect(self.Delete)
-
-
-        PathWindow = QVBoxLayout()
-        PathWindow.addLayout(PathEntrance)
-        PathWindow.addLayout(Buttons)
-        self.setLayout(PathWindow)
-
-        self.setGeometry(300, 300, 400, 150)
-        self.show()
-
-
-    def showPath(self):
-        path = DataBase.FilePath().get_path()
-        for item in path:
-            self.PathList.addItem(item)
-
-
-
-    def RegistPath(self):
-        path = self.PathSpace.text()
-        if path == '': return QMessageBox.critical(self, 'ERROR', 'File Path is empty!')
-
-        file = DataBase.FilePath()
-        file.add_path(path)
-        file.close()
-        self.PathList.clear()
-        self.showPath()
-
-
-
-    def Delete(self):
-        file = DataBase.FilePath()
-        file.clear(self.PathList.currentItem().text())
-        file.close()
-        self.PathList.clear()
-        self.showPath()
-        QMessageBox.information(self, 'Message', 'Deleted Path from Data Base')
-
-
-
+        for item in result:
+            self.Search_result.addItem(item)
 
 
 if __name__ == '__main__':
